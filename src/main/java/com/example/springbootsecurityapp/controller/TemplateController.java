acecb95befd3aa2f09546f378972e4c47bda9307
Usanthan <ukkukkukk@hotmail.com>
2021-09-02 08:46:33 -0400

    clean up

--- src/main/java/com/example/springbootsecurityapp/controller/TemplateController.java
+++ src/main/java/com/example/springbootsecurityapp/controller/TemplateController.java
@@ -1,10 +1,11 @@
 package com.example.springbootsecurityapp.controller;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Profile("form-auth")
 public class TemplateController {
     @GetMapping("/login")
     public String getLoginView() {
